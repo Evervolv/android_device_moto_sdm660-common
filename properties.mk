23f1f7e4268db35cabe0702ea1c00a7a650d72c8
Jeferson <jroliveira.oliveira301@gmail.com>
2022-01-11 13:22:35 +0100

    sdm-660-common: Use skiaglthreaded instead
    Change-Id: I7def214121ebf2f4d0b8854b49f231b9b08e9fa1

--- properties.mk
+++ properties.mk
@@ -106,6 +106,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.hwc_set_default_colormode=true \
+    debug.renderengine.backend=skiaglthreaded \
     debug.sf.enable_hwc_vds=0 \
     debug.sf.enable_egl_image_tracker=true \
     debug.sf.enable_gl_backpressure=1 \
